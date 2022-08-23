ef717fde7efc95f5aaf04c3bec3677e60f4ea1a8
noah willis <n.willis.8724@gmail.com>
2022-08-22 20:11:02 -0500

    commit tree f7bfc6634e7a5622bf518966300f8fa9318c962e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Follow along with the examples here
+function donothing() {}
+function sayHello() {
+    console.log (`Hello!`);
+}
+function sayHelloToGuadalupe(){
+    console.log(`Hello, Guadalupe!`);
+}
+function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+  }
+function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+  }
+  sayHelloToLiz();
+  sayHello();
+  sayHelloToGuadalupe();
+  sayHelloToSamip();
